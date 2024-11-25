fc7f1ca6ffe0231c747a3b0b4556c29d75fcfd5a
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-24 22:28:58 -0400

    files: changing file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:santos_rentacar/features/onboard_screen/screens/onboarding_page.dart';
+import 'package:santos_rentacar/features/onboard_screen/onboarding_page.dart';
 import 'package:santos_rentacar/firebase_options.dart';
 
 Future<void> main() async {
