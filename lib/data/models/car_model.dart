e024074e30ea8e9a26e52c49201c421be8e03177
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-24 22:46:25 -0400

    feat: car structure

---
+++ lib/data/models/car_model.dart
@@ -0,0 +1,18 @@
+class Car {
+  final String model;
+  final double distance;
+  final double fuelCapacity;
+  final double fuelType;
+  final double pricePerHour;
+  final double year;
+  final double color;
+
+  Car(
+      {required this.model,
+      required this.distance,
+      required this.color,
+      required this.fuelCapacity,
+      required this.fuelType,
+      required this.pricePerHour,
+      required this.year});
+}
