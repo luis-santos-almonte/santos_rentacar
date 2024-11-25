5a6ffb6eb75853f2c3c5ff0e27baf86c592b9dea
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-24 22:22:36 -0400

    feat: final onboarding design

--- lib/features/onboard_screen/screens/onboarding_page.dart
+++ lib/features/onboard_screen/screens/onboarding_page.dart
@@ -1,26 +1,66 @@
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
-  const OnboardingPage({super.key});
+  const OnboardingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/onboarding.png'),
-                  fit: BoxFit.cover)),
-        ),
-        Container(
-            child: Column(
-          children: [
-            Text('Wherever life takes you, \nlet us drive you there.')
-          ],
-        ))
-      ],
-    ));
+      backgroundColor: Color(0xff2C2B34),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/onboarding.png'),
+                      fit: BoxFit.fitWidth)),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Premium cars. \nEnjoy the luxury',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.white),
+                        child: Text(
+                          'Let\'s Go',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        )),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
