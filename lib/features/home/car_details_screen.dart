79486b4b4e2bddefb03617fcd514ed571e63cd13
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-25 22:57:17 -0400

    feat: Car presentation

--- lib/features/home/car_details_screen.dart
+++ lib/features/home/car_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:santos_rentacar/data/models/car_model.dart';
+import 'package:santos_rentacar/widgets/car_card.dart';
 
 class CarDetailsScreen extends StatelessWidget {
   const CarDetailsScreen({super.key});
@@ -8,12 +10,81 @@ class CarDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Icon(Icons.info_outline),
-          Text('Information')],
+          children: [Icon(Icons.info_outline), Text('Information')],
         ),
       ),
       body: Column(
-        children: [],
+        children: [
+          CarCard(
+              car: Car(
+                  model: 'Fortuner GR',
+                  distance: 870,
+                  color: 'black',
+                  fuelCapacity: 50,
+                  fuelType: 'Gasoline',
+                  pricePerHour: 45,
+                  year: 2024)),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Color(0xffF3F3F3),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10,
+                              spreadRadius: 5)
+                        ]),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage('assets/images/user.png'),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Jane Cooper',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '\$4.253',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: Container(
+                    height: 170,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/maps.png'),
+                            fit: BoxFit.cover),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10,
+                              spreadRadius: 5)
+                        ]),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
