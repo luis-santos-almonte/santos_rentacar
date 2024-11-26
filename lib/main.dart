bc32d76532e4e2a64a4da8e9f7eff3920c465421
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-25 22:19:32 -0400

    feat: Car details screen initial configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:santos_rentacar/features/home/car_details_screen.dart';
 import 'package:santos_rentacar/features/onboard_screen/onboarding_page.dart';
 import 'package:santos_rentacar/firebase_options.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnboardingPage(),
+      home: const CarDetailsScreen(),
     );
   }
 }
