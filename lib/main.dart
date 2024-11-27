16d05169b2be85a8d629b3b94cd378f9837ff8d5
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-26 22:45:01 -0400

    feat: Creating car data structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:santos_rentacar/features/onboard_screen/onboarding_page.dart';
+import 'package:santos_rentacar/features/pages/onboard_screen/onboarding_page.dart';
 import 'package:santos_rentacar/firebase_options.dart';
 
 Future<void> main() async {
