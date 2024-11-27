e16c34284f32b385d598c14d0399c137dd6d05b3
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-26 22:59:02 -0400

    feat: Initializing data structure

---
+++ lib/injection_container.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
+import 'package:santos_rentacar/data/datasources/firebase_car_data_source.dart';
+import 'package:santos_rentacar/data/repositories/car_repository_impl.dart';
+import 'package:santos_rentacar/domain/repositories/car_repository.dart';
+import 'package:santos_rentacar/domain/usecases/get_cars.dart';
+import 'package:santos_rentacar/features/bloc/car_bloc.dart';
+
+GetIt getIt = GetIt.instance;
+
+void initInjection() {
+  try {
+    getIt.registerLazySingleton(() => FirebaseFirestore.instance);
+    getIt.registerLazySingleton<FirebaseCarDataSource>(
+        () => FirebaseCarDataSource(firestore: getIt<FirebaseFirestore>()));
+    getIt.registerLazySingleton<CarRepository>(
+        () => CarRepositoryImpl(getIt<FirebaseCarDataSource>()));
+    getIt.registerLazySingleton<GetCars>(() => GetCars(getIt<CarRepository>()));
+    getIt.registerFactory(() => CarBloc(getCars: getIt<GetCars>()));
+  } catch (e) {
+    throw e;
+  }
+}
